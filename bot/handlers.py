6dcf775fff0c0dfa29fb5f796f4cb2ed9fd56f1c
GStorm888 <denchikevstr@icloud.com>
2025-07-01 13:01:49 +0300

    добавил хелп с иерархией прав, добавил базовые функции в БД.
    есть баг с тем что надо переделать датаклассы и БД по расстоновке полей

--- bot/handlers.py
+++ bot/handlers.py
@@ -5,6 +5,7 @@ import datetime
 
 
 def register_handlers(bot: TeleBot):
+    
     def examination_date_type(user_time):
         time_format = "%H:%M"
         try:
@@ -12,6 +13,12 @@ def register_handlers(bot: TeleBot):
             return True
         except:
             return False
+
+    @bot.message_handler(func=lambda message: message.text == "🔙Назад")
+    def back_button(message):
+        bot.send_message(message.chat.id, "Хорошо, возвращаю вас в Help")
+        help(message)
+
 
     # @bot.message_handler(commands=["test"])
     # def test(message):
@@ -64,7 +71,89 @@ def register_handlers(bot: TeleBot):
             reply_markup=markup,
         )
 
-    @bot.message_handler(func=lambda message: message.text == "🔙Назад")
-    def back_button(message):
-        bot.send_message(message.chat.id, "Хорошо, возвращаю вас в Help")
-        help(message)
+
+
+
+    @bot.message_handler(commands=["help"])
+    def help(message):
+        telegram_id = str(message.chat.id)
+        user = Database.get_user_from_users_by_telegram_id(telegram_id)
+        admin = Database.get_admin_from_admin_by_telegram_id(telegram_id)
+        markup = types.InlineKeyboardMarkup()
+        if user is not None:
+            help_bttn = types.InlineKeyboardButton(text="🆘help", callback_data="help")
+            start_bttn = types.InlineKeyboardButton(text="🚀start", callback_data="start")
+
+            view_lessons_bttn = types.InlineKeyboardButton(
+                text="просмотр всех занятий на этой неделе", callback_data="view_lessons"
+            )
+            view_next_lesson_bttn = types.InlineKeyboardButton(text="просмотр ближайшего занятия", callback_data="view_next_lesson")
+
+            rename_bttn = types.InlineKeyboardButton(
+                text="изменить имя пользователя", callback_data="rename"
+            )
+            markup.add(help_bttn, start_bttn)
+            markup.add(view_lessons_bttn, view_next_lesson_bttn)
+            markup.add(rename_bttn)
+
+        if admin.status_admin == 1:
+
+            add_lesson_bttn = types.InlineKeyboardButton(
+                text="добавить занятие", callback_data="add_lesson"
+            )
+
+
+            change_lesson_bttn = types.InlineKeyboardButton(
+                text="изменить занятие", callback_data="change_lesson"
+            )
+
+            delete_lesson_bttn = types.InlineKeyboardButton(
+                text="отмена занятия", callback_data="delete_lesson"
+            )
+            reschedule_lesson_bttn = types.InlineKeyboardButton(
+                text="перенос занятия", callback_data="reschedule_lesson"
+            )
+
+            view_my_reschedule_lesson_bttn = types.InlineKeyboardButton(text="просмотр моих переносов", callback_data="view_my_reschedule_lesson")
+            markup.add(add_lesson_bttn)
+            markup.add(change_lesson_bttn)
+            markup.add(delete_lesson_bttn)
+            markup.add(reschedule_lesson_bttn)
+            markup.add(view_my_reschedule_lesson_bttn)
+
+            if admin.status_admin == 2:
+
+
+                view_eschedule_lessons_bttn = types.InlineKeyboardButton(
+                    text="просмотр всех переносов", callback_data="view_eschedule_lessons"
+                )
+
+                send_message_studends_bttn = types.InlineKeyboardButton(
+                    text="отправка сообщения всем ученикам", callback_data="send_message_studends"
+                )
+
+                send_message_teachers_bttn = types.InlineKeyboardButton(
+                    text="отправка сообщения всем учителям", callback_data="send_message_teachers"
+                )
+
+                add_user_bttn = types.InlineKeyboardButton(
+                    text="добавить пользователя", callback_data="add_user"
+                )
+                markup.add(view_eschedule_lessons_bttn)
+                markup.add(send_message_studends_bttn)
+                markup.add(send_message_teachers_bttn)
+                markup.add(add_user_bttn)
+
+                if admin_status_admin == 3:
+
+                    chek_db = types.InlineKeyboardButton(
+                        text="просмотр БД", callback_data="chek_db"
+                    )
+                    markup.add(chek_db)
+
+    bot.send_message(
+        message.chat.id,
+        "в кнопках все команды и функции",
+        reply_markup=markup,
+    )
+    markup.add(delete_account_bttn)
--- db/db.py
+++ db/db.py
@@ -79,3 +79,121 @@ class Database:
             ],
         )
         return True
+
+
+
+    @staticmethod  # получение всех пользователей
+    def get_all_users():
+        connection = sqlite3.connect(Database.DATABASE)
+
+        cursor = connection.cursor()
+
+        cursor.execute("SELECT * FROM users")
+
+        all_users = cursor.fetchall()
+        users = []
+        for id, const_user_name, user_user_name, telegram_id, name_teacher in all_users:
+            user = User(const_user_name, user_user_name, telegram_id, name_teacher, id)
+            users.append(user)
+        if len(users) == 0:
+            return None
+        return users
+
+
+    @staticmethod  # получение всех пользователей
+    def get_all_lessons():
+        connection = sqlite3.connect(Database.DATABASE)
+
+        cursor = connection.cursor()
+
+        cursor.execute("SELECT * FROM lessons")
+
+        all_lessons = cursor.fetchall()
+        lessons = []
+        for id, telegram_id, day_lesson, time_lesson, time_reminder, name_teacher in all_lessons:
+            lesson = Lesson(telegram_id, day_lesson, time_lesson, time_reminder, name_teacher, id)
+            lessons.append(lessons)
+        if len(lessons) == 0:
+            return None
+        return lessons
+
+    @staticmethod  # получение всех пользователей
+    def get_all_change_lessons():
+        connection = sqlite3.connect(Database.DATABASE)
+
+        cursor = connection.cursor()
+
+        cursor.execute("SELECT * FROM change_lessons")
+
+        all_change_lessons = cursor.fetchall()
+        change_lessons = []
+        for id, telegram_id, day_lesson, old_time_lesson, new_time_lesson, time_reminder, name_teacher in all_change_lessons:
+            change_lesson = ChangeLesson(telegram_id, day_lesson, old_time_lesson, new_time_lesson, time_reminder, name_teacher, id)
+            change_lessons.append(change_lesson)
+        if len(change_lessons) == 0:
+            return None
+        return change_lessons
+
+
+    @staticmethod  # получение всех пользователей
+    def get_all_admins():
+        connection = sqlite3.connect(Database.DATABASE)
+
+        cursor = connection.cursor()
+
+        cursor.execute("SELECT * FROM admins")
+
+        all_admins = cursor.fetchall()
+        admins = []
+        for id, name_admin, password_admin, telegram_id, status_admin in all_admins:
+            admin = Admin(name_admin, password_admin, telegram_id, status_admin, id)
+            admins.append(admin)
+        if len(admins) == 0:
+            return None
+        return admins
+
+    
+    @staticmethod
+    def get_user_from_users_by_telegram_id(telegram_id):
+        connection = sqlite3.connect(Database.DATABASE, check_same_thread=False)
+
+        cursor = connection.cursor()
+
+        cursor.execute(
+            "SELECT * FROM users WHERE telegram_id = ?", [telegram_id]
+        )
+        users = cursor.fetchall()
+
+        if len(users) == 0:
+            return None
+
+        id, const_user_name, user_user_name, telegram_id, name_teacher = users[0]
+        user = User(const_user_name, user_user_name, telegram_id, name_teacher, id)
+        return user
+
+
+    @staticmethod 
+    def change_telegram_id_in_users_by_user_name(telegram_id, const_user_name):
+        Database.execute(
+            "UPDATE users SET telegram_id=? WHERE const_user_name=?",
+            [telegram_id, const_user_name],
+        )
+        return True
+
+    @staticmethod
+    def get_admin_from_admin_by_telegram_id(telegram_id)
+        connection = sqlite3.connect(Database.DATABASE, check_same_thread=False)
+
+        cursor = connection.cursor()
+
+        cursor.execute(
+            "SELECT * FROM admins WHERE telegram_id = ?", [telegram_id]
+        )
+        admins = cursor.fetchall()
+
+        if len(admins) == 0:
+            return None
+
+        id, name_admin, password_admin, telegram_id, status_admin = admins[0]
+        admin = Admins(name_admin, password_admin, telegram_id, status_admin, id)
+        return admin
